4d4633755d1d0a5b7006e338f47276fec4882161
Damian Mooyman <damian@silverstripe.com>
2016-02-10 17:11:08 +1300

    Extra module name rules

--- src/Model/Module.php
+++ src/Model/Module.php
@@ -132,8 +132,15 @@ class Module
     public function getTeam()
     {
         switch ($this->name) {
+            case 'queuedjobs':
+            case 'advancedworkflow':
+                return 'silverstripe-australia';
+            case 'fulltextsearch':
             case 'reports':
+            case 'tagfield':
                 return 'silverstripe-labs';
+            case 'gridfield-bulk-editing-tools':
+                return 'colymba';
             default:
                 return 'silverstripe';
         }
@@ -152,6 +159,16 @@ class Module
         // Use gitlab for cwp repos
         if(stripos($name, 'cwp') === 0) {
             return "https://gitlab.cwp.govt.nz/cwp/{$name}/";
+        }
+
+        if($name === 'gridfield-bulk-editing-tools') {
+            return "https://github.com/colymba/GridFieldBulkEditingTools/";
+        }
+
+        switch($name) {
+            case 'advancedworkflow':
+                return "https://github.com/{$team}/{$name}/";
+                break;
         }
         
         return "https://github.com/{$team}/silverstripe-{$name}/";
